53d2b1d2f6d4210cb9365ce6d07efc1aecc436e6
tushar swami <you@example.com>
2024-03-10 23:55:44 -0700

    change routes

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -2,9 +2,11 @@ import express from "express";
 const router = express.Router();
 import { User } from "../model/user.model.js";
 import { customerDetail } from "../controllers/customer.js";
-router.get("/", async (req, res) => {
-  const user = await User.find();
-  return res.json(user);
-}).get('/customer/:id',customerDetail);
+router
+  .get("/allCustomer", async (req, res) => {
+    const user = await User.find();
+    return res.json(user);
+  })
+  .get("/customer/:id", customerDetail);
 
 export { router };
