1ab26ec0514e7fbffd7c62e6804dc98d09055b0b
tushar swami <you@example.com>
2024-03-24 15:22:16 +0530

    change in mongoose

--- server/config/mongoose.connect.js
+++ server/config/mongoose.connect.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const password = process.env.PASSWORD;
 main().catch((err) => console.error(`error in mongoose connection ${err}`));
 async function main() {
   await mongoose.connect(
-    "mongodb+srv://swamitusharjee2018:tusharswami12345678@cluster0.2h4sx8f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    `mongodb+srv://swamitusharjee2018:${password}@cluster0.2h4sx8f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
   );
   console.log("mongoose connected");
 }
